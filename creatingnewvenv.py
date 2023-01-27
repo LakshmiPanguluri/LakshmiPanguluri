e099d509cf9f1d6a886d9039d3ec659681d1f083
Lakshmi Durga Panguluri <lakshmidurga62@gmail.com>
2023-01-27 15:43:08 -0500

    creating new env

---
+++ creatingnewvenv.py
@@ -0,0 +1,8 @@
+#To install a new envioronment
+
+conda create -n langchain python=3.9.0
+conda activate langchain
+which pip
+export PATH: location to conda/envs/langchain/bin:$PATH
+conda install jupyter 
+python -m ipykernel install --user --name=langchain
